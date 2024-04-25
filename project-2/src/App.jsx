4893987afacda8a234ef7d0290b16d74cb0b57e0
ankon35 <ankon35744@gmail.com>
2024-04-25 12:24:40 +0600

    Nav and Header section complete

---
+++ project-2/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import ContactHeader from './component/ConatctHeader/ContactHeader'
+import Navigation from './component/Navigation/Navigation'
+
+function App() {
+  
+
+  return (
+    <div>
+      <Navigation />
+      <ContactHeader />
+    </div>
+  )
+}
+
+export default App
---
+++ project-2/src/component/ConatctHeader/ContactHeader.jsx
@@ -0,0 +1,15 @@
+
+import styles from "./ContactHeader.module.css";
+
+const ContactHeader = () => {
+  return (
+    <div className= {
+        `${styles.header} container`
+    }>
+        <h1>Contact Page</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus repudiandae ea, nisi beatae praesentium vitae, quo eum ducimus quas, consequuntur inventore. Minima id accusantium amet est nisi eos autem iure.</p>
+    </div>
+  )
+}
+
+export default ContactHeader
